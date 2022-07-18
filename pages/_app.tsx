1b3f5be37688517d8138a73bb1da952a4596297c
Julien tremblay <j.tremblay@mesdocteurs.com>
2022-07-18 21:51:09 +0200

    fix tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "styles/globals.css";
+import "styles/theme/custom.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../styles/theme/theme";
